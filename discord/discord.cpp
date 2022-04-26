70625b1df1f4bb0846eed9af8315eac5eac9bcbe
ads00 <ads.neuroshok@gmail.com>
2022-04-26 11:55:25 +0200

    [test]

--- discord/discord.cpp
+++ discord/discord.cpp
@@ -1,11 +1,9 @@
 #include "discord.hpp"
 
 #include <nxi/core.hpp>
-#include <nxi/system/window.hpp>
 #include <nxi/log.hpp>
+#include <nxi/system/window.hpp>
 
-
-#include <QString>
 #include <array>
 #include <cassert>
 #include <csignal>
@@ -14,151 +12,9 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <QString>
 
 #include "source/discord.h"
-
-static std::string data;
-
-#if defined(_WIN32)
-#pragma pack(push, 1)
-struct BitmapImageHeader {
-    uint32_t const structSize{sizeof(BitmapImageHeader)};
-    int32_t width{0};
-    int32_t height{0};
-    uint16_t const planes{1};
-    uint16_t const bpp{32};
-    uint32_t const pad0{0};
-    uint32_t const pad1{0};
-    uint32_t const hres{2835};
-    uint32_t const vres{2835};
-    uint32_t const pad4{0};
-    uint32_t const pad5{0};
-
-    BitmapImageHeader& operator=(BitmapImageHeader const&) = delete;
-};
-
-struct BitmapFileHeader {
-    uint8_t const magic0{'B'};
-    uint8_t const magic1{'M'};
-    uint32_t size{0};
-    uint32_t const pad{0};
-    uint32_t const offset{sizeof(BitmapFileHeader) + sizeof(BitmapImageHeader)};
-
-    BitmapFileHeader& operator=(BitmapFileHeader const&) = delete;
-};
-#pragma pack(pop)
-#endif
-
-struct DiscordState {
-    discord::User currentUser;
-
-    std::unique_ptr<discord::Core> core;
-};
-
-namespace {
-volatile bool interrupted{false};
-}
-
-
-void f()
-{
-    DiscordState state{};
-    DiscordCreateParams p{};
-    discord::Core* core{};
-    auto result = discord::Core::Create(807065370573668372, DiscordCreateFlags_Default, &core);
-    state.core.reset(core);
-    if (!state.core)
-    {
-        std::cout << "Failed to instantiate discord core! (err " << static_cast<int>(result)
-                  << ")\n";
-        std::exit(-1);
-    }
-
-    state.core->SetLogHook(
-      discord::LogLevel::Debug, [](discord::LogLevel level, const char* message) {
-          std::cerr << "Log(" << static_cast<uint32_t>(level) << "): " << message << "\n";
-      });
-
-
-    discord::User user;
-    state.core->RelationshipManager().OnRefresh.Connect([&state]()
-    {
-          std::cout << "refresh\n";
-        state.core->RelationshipManager().Filter([](const discord::Relationship& rel) -> bool
-        {
-                                                       data += rel.GetUser().GetUsername() + std::string("\n");
-              std::cout << rel.GetUser().GetUsername() << "\n";
-              return true;
-        });
-    });
-    state.core->RelationshipManager().OnRelationshipUpdate.Connect([](const discord::Relationship& rel)
-    {
-          std::cout << rel.GetUser().GetUsername() << "\n";
-    });
-
-
-
-
-
-    discord::LobbyTransaction l;
-    state.core->LobbyManager().GetLobbyCreateTransaction(&l);
-    l.SetCapacity(10);
-    l.SetType(discord::LobbyType::Public);
-
-    state.core->ActivityManager().OnActivityInvite.Connect([](discord::ActivityActionType, discord::User const &, discord::Activity const &) {
-        std::cout << "accept";
-        system("D:\\project\\nk\\nxi\\bin\\nxi\\nxi.exe");
-    });
-
-    /*
-    state.core->LobbyManager().CreateLobby(l, [&state](discord::Result result, discord::Lobby const& lobby)
-    {
-        discord::Activity activity{};
-        activity.SetName("zeta");
-        activity.SetType(discord::ActivityType::Playing);
-        activity.SetState("nxi");
-        activity.SetDetails("[sniper][bomberman]");
-        activity.GetTimestamps().SetStart(std::time(nullptr));
-        activity.GetTimestamps().SetEnd(std::time(nullptr) + 1000);
-        activity.GetAssets().SetLargeImage("large");
-        activity.GetAssets().SetSmallImage("large");
-        activity.GetAssets().SetLargeText("__");
-        activity.GetAssets().SetSmallText("__");
-        activity.GetParty().SetId("200");
-        activity.GetParty().GetSize().SetCurrentSize(3);
-        activity.GetParty().GetSize().SetMaxSize(10);
-        activity.GetSecrets().SetJoin(lobby.GetSecret());
-        activity.GetSecrets().SetSpectate("lobby.GetSecret()");
-        //activity.GetSecrets().SetMatch(lobby.GetSecret());
-
-        state.core->ActivityManager().UpdateActivity(activity, [](discord::Result result)
-                                                     {
-                                                         std::cout << "ok";
-                                                     });
-        state.core->ActivityManager().SendInvite(820766957171245077, discord::ActivityActionType::Join,"", [](discord::Result result)
-        {
-            std::cout << (int)result;
-        });
-
-        state.core->ActivityManager().AcceptInvite(820766957171245077, [](discord::Result)
-                                                   {
-                                                       system("D:\\project\\nk\\nxi\\bin\\nxi\\nxi.exe");
-                                                   });
-        state.core->ActivityManager().RegisterCommand("nxi");
-
-    });*/
-
-
-
-
-
-    do {
-        state.core->RunCallbacks();
-
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
-    } while (true);
-}
 
 namespace nxi::modules
 {
@@ -169,15 +25,112 @@ namespace nxi::modules
 
     void discord::on_load()
     {
-        auto cmd = nxi::command("discord", "friend", [this](const nxi::values&)
-        {
-            session_.error(QString::fromStdString(::data));
-        });
+        nxi::command_data share;
+        share.module = "discord";
+        share.action = "share";
+        share.description = "Share this page with a discord friend";
+        share.function = [this](const nxi::values& values) {
+            nxi_assert(values.size() > 1);
+            auto id = values.get(1).toLongLong();
+            send_invite(id);
+        };
 
-        session_.command_system().add(std::move(cmd));
+        share.parameters = { { "friend_name", [this](nxi::suggestion_vector& suggestion) {
+                                  for (auto f: friends_) suggestion.push_back(f);
+                              } },
+                             { "id" } };
 
-        std::thread t(&f);
+        session_.command_system().add(std::move(share));
+
+        auto f = [this] {
+            do
+            {
+                state.core->RunCallbacks();
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(16));
+            } while (true);
+        };
+
+        init();
+
+        std::thread t(f);
         t.detach();
     }
 
-} // nxi::modules
+    void discord::init()
+    {
+        auto result = ::discord::Core::Create(807065370573668372, DiscordCreateFlags_Default, &core);
+        state.core.reset(core);
+        if (!state.core)
+        {
+            std::cout << "Failed to instantiate discord core! (err " << static_cast<int>(result) << ")\n";
+            std::exit(-1);
+        }
+
+        state.core->SetLogHook(::discord::LogLevel::Debug, [](::discord::LogLevel level, const char* message) {
+            std::cerr << "Log(" << static_cast<uint32_t>(level) << "): " << message << "\n";
+        });
+
+        ::discord::User user;
+        state.core->RelationshipManager().OnRefresh.Connect([this]() {
+            state.core->RelationshipManager().Filter([this](const ::discord::Relationship& rel) -> bool {
+                contact c;
+                c.id = rel.GetUser().GetId();
+                c.name = rel.GetUser().GetUsername();
+                friends_.push_back(discord_suggestion{ c.id, QString::fromStdString(c.name) });
+                return true;
+            });
+        });
+        /*
+        state.core->RelationshipManager().OnRelationshipUpdate.Connect(
+        [](const ::discord::Relationship& rel) { std::cout << rel.GetUser().GetUsername() << "\n"; });*/
+
+        state.core->ActivityManager().OnActivityInvite.Connect([](::discord::ActivityActionType, ::discord::User const&, ::discord::Activity const&) {
+            std::cout << "accept";
+            // system("D:\\project\\nk\\nxi\\bin\\nxi\\nxi.exe");
+        });
+
+        state.core->ActivityManager().AcceptInvite(820766957171245077, [](::discord::Result) { system("D:\\project\\nk\\nxi\\bin\\nxi\\nxi.exe"); });
+        state.core->ActivityManager().RegisterCommand("nxi");
+    }
+
+    void discord::send_invite(::discord::UserId id)
+    {
+        ::discord::LobbyTransaction l;
+        state.core->LobbyManager().GetLobbyCreateTransaction(&l);
+        l.SetCapacity(10);
+        l.SetType(::discord::LobbyType::Public);
+
+        auto url = session_.page_system().focus()->command();
+        ::discord::User self;
+        state.core->UserManager().GetCurrentUser(&self);
+        invite_message_ = "**" + QString{ self.GetUsername() } + "** shared with you this page from **nxi browser**\n<" + url + ">";
+
+
+        state.core->LobbyManager().CreateLobby(l, [id, this](::discord::Result result, ::discord::Lobby const& lobby) {
+            ::discord::Activity activity{};
+            activity.SetName("zeta");
+            activity.SetType(::discord::ActivityType::Playing);
+            activity.SetState("nxi");
+            activity.SetDetails("[details][]");
+            activity.GetTimestamps().SetStart(std::time(nullptr));
+            activity.GetTimestamps().SetEnd(std::time(nullptr) + 1000);
+            activity.GetAssets().SetLargeImage("large");
+            activity.GetAssets().SetSmallImage("large");
+            activity.GetAssets().SetLargeText("__");
+            activity.GetAssets().SetSmallText("__");
+            activity.GetParty().SetId("200");
+            activity.GetParty().GetSize().SetCurrentSize(3);
+            activity.GetParty().GetSize().SetMaxSize(10);
+            activity.GetSecrets().SetJoin(lobby.GetSecret());
+            activity.GetSecrets().SetSpectate("lobby.GetSecret()");
+            // activity.GetSecrets().SetMatch(lobby.GetSecret());
+
+            state.core->ActivityManager().UpdateActivity(activity, [](::discord::Result result) { std::cout << "ok"; });
+
+            //session_.error(id.toULongLong());
+            state.core->ActivityManager().SendInvite(id, ::discord::ActivityActionType::Join, invite_message_.toStdString().c_str(),
+                                                     [this](::discord::Result result){ session_.error("invite sent"); });
+        });
+    }
+} // namespace nxi::modules
--- discord/discord.hpp
+++ discord/discord.hpp
@@ -2,6 +2,46 @@
 #define EXP_MODULE_TEST_H_NXI
 
 #include <nxi/module.hpp>
+#include <vector>
+#include <nxi/suggestion/vector.hpp>
+#include "source/discord.h"
+
+
+#if defined(_WIN32)
+#pragma pack(push, 1)
+struct BitmapImageHeader {
+    uint32_t const structSize{sizeof(BitmapImageHeader)};
+    int32_t width{0};
+    int32_t height{0};
+    uint16_t const planes{1};
+    uint16_t const bpp{32};
+    uint32_t const pad0{0};
+    uint32_t const pad1{0};
+    uint32_t const hres{2835};
+    uint32_t const vres{2835};
+    uint32_t const pad4{0};
+    uint32_t const pad5{0};
+
+    BitmapImageHeader& operator=(BitmapImageHeader const&) = delete;
+};
+
+struct BitmapFileHeader {
+    uint8_t const magic0{'B'};
+    uint8_t const magic1{'M'};
+    uint32_t size{0};
+    uint32_t const pad{0};
+    uint32_t const offset{sizeof(BitmapFileHeader) + sizeof(BitmapImageHeader)};
+
+    BitmapFileHeader& operator=(BitmapFileHeader const&) = delete;
+};
+#pragma pack(pop)
+#endif
+
+struct DiscordState {
+    discord::User currentUser;
+
+    std::unique_ptr<discord::Core> core;
+};
 
 namespace nxi { class session; }
 
@@ -9,14 +49,38 @@ namespace nxi::modules
 {
     class discord : public nxi::module
     {
+        struct contact
+        {
+            ::discord::UserId id;
+            std::string name;
+        };
+
+        struct discord_suggestion : text_suggestion
+        {
+            discord_suggestion(::discord::UserId user_id_, QString name)
+                : text_suggestion(std::move(name))
+                , user_id{ user_id_ }
+            {}
+            ::discord::UserId user_id;
+        };
+
     public:
         explicit discord(nxi::session&);
 
         void on_load();
+        void init();
+        void send_invite(::discord::UserId);
 
     private:
         nxi::session& session_;
+        QString invite_message_;
 
+        //std::vector<contact> friends_;
+        std::vector<discord_suggestion> friends_;
+
+        DiscordState state{};
+        DiscordCreateParams p{};
+        ::discord::Core* core{};
     };
 } // nxi::modules
 
